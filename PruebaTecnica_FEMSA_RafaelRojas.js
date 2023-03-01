a479c9005ccb30c86541e4c179f0794088480794
rafaelrojas92 <rafaelrojas92@gmail.com>
2023-03-01 17:49:52 -0300

    Prueba técnica

---
+++ PruebaTecnica_FEMSA_RafaelRojas.js
@@ -0,0 +1,29 @@
+
+//Validations:
+const validLength = word => !!word && word.length >= 4 && word.length <= 20
+
+const beginsWithLetter = word => !!word && /^[a-zA-Z]/.test(word)
+
+const lettersNumbersUnderscoreOnly = word => !!word && /^[a-zA-Z0-9_]+$/.test(word)
+
+const endsWithoutUnderscore = word => !!word && !word.endsWith("_")
+
+//Main function:
+const CodelandUsernameValidation = username =>
+    validLength(username) &&
+    beginsWithLetter(username) &&
+    lettersNumbersUnderscoreOnly(username) &&
+    endsWithoutUnderscore(username)
+
+//User input:
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Please enter your Username: ", username => {
+
+    console.log(`Output: ${CodelandUsernameValidation(username) ? "verdadero" : "falso"}`);
+
+    readline.close();
+});
---
+++ README.md
@@ -0,0 +1,20 @@
+# Prueba Técnica FEMSA
+
+Prueba técnica para proceso de selección de FEMSA, desarrollado por Rafael Rojas Santander
+
+## Instalación
+
+Se debe tener instalado Node para ejecutar el código (Se usó la versión 16.13.1 para el desarrollo)
+
+## Uso
+
+En una terminal, se debe estar en la misma carpeta que contenga el archivo PruebaTecnica_FEMSA_RafaelRojas.js, y ejecutar:
+```
+node PruebaTecnica_FEMSA_RafaelRojas.js
+```
+Luego, en la misma terminal, se solicitará el nombre de usuario. Se debe ingresar y presionar Enter.
+
+El resultado aparecerá en una nueva línea, con el formato
+```
+Salida: {verdadero o falso}
+```
